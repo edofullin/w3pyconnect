50a8c3e9bf12004960746b5c00cc6aecccf04a09
Edoardo Fullin <edoardo.fullin@outlook.com>
2022-08-31 21:39:53 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="w3_pyconnect",
+    version="0.1.0",
+    author="Edoardo Fullin",
+    author_email="edoardo.fullin@outlook.com",
+    description="Wind3 API Wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/edofullin/w3_pyconnect",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
